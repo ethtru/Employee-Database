c0e3d33a6502cd4f5b8d7d3bd80d1472381cfcbe
Eth <ethtru@gmail.com>
2023-09-20 19:31:05 -0400

    fixed the seeds bug!

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
@@ -17,20 +17,6 @@ CREATE TABLE role (
   REFERENCES department(id)
   ON DELETE CASCADE
 );
-
--- CREATE TABLE employee (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   role_id INT NOT NULL,
---   FOREIGN KEY (role_id)
---   REFERENCES role(id)
---   ON DELETE CASCADE,
---   manager_id INT,
---   FOREIGN KEY (manager_id)
---   REFERENCES employee(id)
---   ON DELETE SET NULL
--- );
 
 CREATE TABLE employee (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
